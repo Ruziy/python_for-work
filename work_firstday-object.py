988fea348e36238597de5b8c568d1a94305b6cf5
Alex <razuvaev.sasha2015@yandex.ru>
2023-07-25 11:17:54 +0400

    Input for find-object,more tests.

--- work_firstday-object.py
+++ work_firstday-object.py
@@ -1,15 +1,18 @@
 import cv2 
 import numpy as np
-cap = cv2.VideoCapture('videos/videos-object-3.mkv')
+cap = cv2.VideoCapture('videos/videos-object-4.mkv')
+choose = input('Choose num peaks')
+choose = int(choose)
 def nothing(x):
     pass
 cv2.namedWindow('Track')
 cv2.createTrackbar('T1','Track',0,255,nothing)
+cv2.setTrackbarPos('T1','Track',255)
 cv2.createTrackbar('T2','Track',0,255,nothing)
 kernel = np.ones((5,5))
 while True:
     succes,img = cap.read()
-    img = cv2.resize(img,(600,400))
+    img = cv2.resize(img,(1500,1000))
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     thresh1 = cv2.getTrackbarPos('T1','Track')
     thresh2 = cv2.getTrackbarPos('T2','Track')
@@ -24,8 +27,8 @@ while True:
             p = cv2.arcLength(contour,True)
             num = cv2.approxPolyDP(contour,0.01*p,True)
             x,y,w,h = cv2.boundingRect(num)
-            if len(num) == 3:
-                cv2.rectangle(img,(x,y,x+w,y+h),(0,0,255),4)            
+            if len(num) == choose:
+                cv2.rectangle(img,(x,y,x+w,y+h),(0,0,255),3)           
     cv2.imshow('canny',canny)
     cv2.imshow('dill',dill)
     cv2.imshow('img',img)

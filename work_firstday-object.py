42982a2f0c9b48a99c046d54c88bc8325f0f4512
Alex <razuvaev.sasha2015@yandex.ru>
2023-07-30 03:33:44 +0400

    Find dz for help hands

--- work_firstday-object.py
+++ work_firstday-object.py
@@ -11,6 +11,13 @@ cv2.createTrackbar('T1','Track',0,255,nothing)
 cv2.setTrackbarPos('T1','Track',24)
 cv2.createTrackbar('T2','Track',0,255,nothing)
 cv2.setTrackbarPos('T2','Track',33)
+
+cv2.createTrackbar('T3','Track',0,800,nothing)
+
+cv2.createTrackbar('T4','Track',0,600,nothing)
+cv2.createTrackbar('T5','Track',0,800,nothing)
+
+cv2.createTrackbar('T6','Track',0,600,nothing)
 kernel = np.ones((4,4))
 while True:
     succes,img = cap.read()
@@ -19,18 +26,27 @@ while True:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     thresh1 = cv2.getTrackbarPos('T1','Track')
     thresh2 = cv2.getTrackbarPos('T2','Track')
+    thresh3 = cv2.getTrackbarPos('T3','Track')
+    thresh4 = cv2.getTrackbarPos('T4','Track')
+    thresh5 = cv2.getTrackbarPos('T5','Track')
+    thresh6 = cv2.getTrackbarPos('T6','Track')
     canny = cv2.Canny(gray,thresh1,thresh2)
     dill = cv2.dilate(canny,kernel,iterations=2)
     new_img = img.copy()
     contours,h = cv2.findContours(dill,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         area = cv2.contourArea(contour)
+       
         if area > 10000:
-            cv2.drawContours(new_img,contour,-1,(200,200,0),3)
+            contours = cv2.drawContours(new_img,contour,-1,(200,200,0),3)
+            print(contours)
             p = cv2.arcLength(contour,True)
             # print(p)
-            num = cv2.approxPolyDP(contour,0.3*p,True)
-            cv2.line(img,(520,200),(526,193),(119,201,105),thickness=3)
+            num = cv2.approxPolyDP(contour,3*p,True)
+            cv2.line(img,(514,193),(610,390),(119,201,105),thickness=3)
+            cv2.line(img,(223,347),(316,543),(119,201,105),thickness=3)
+            cv2.line(img,(223,347),(514,193),(119,201,105),thickness=3)
+            cv2.line(img,(316,543),(610,390),(119,201,105),thickness=3)
             x,y,w,h = cv2.boundingRect(num)
             if len(num) == choose:
                 print(x,y,w,h)
@@ -38,7 +54,6 @@ while True:
                 parall = ceil(atan(abs((x-y)/1+x*y))*180/pi)
                 if choose == 4 and parall == 90:
                     cv2.putText(img,'90 degree',(x+w//4,y+h+h//6),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
-    print(max(contour[0][0]))
     Hori = np.concatenate((dill, gray), axis=1)
     Hori_2 = np.concatenate((new_img,img), axis=1)
     cv2.imshow('One layer', Hori)

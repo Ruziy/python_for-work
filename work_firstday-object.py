905015b51ebf0ec8131f81988e68b5b791417ea2
Alex <razuvaev.sasha2015@yandex.ru>
2023-07-30 21:24:18 +0400

    Resolve for dz

--- work_firstday-object.py
+++ work_firstday-object.py
@@ -11,12 +11,9 @@ cv2.createTrackbar('T1','Track',0,255,nothing)
 cv2.setTrackbarPos('T1','Track',24)
 cv2.createTrackbar('T2','Track',0,255,nothing)
 cv2.setTrackbarPos('T2','Track',33)
-
 cv2.createTrackbar('T3','Track',0,800,nothing)
-
 cv2.createTrackbar('T4','Track',0,600,nothing)
 cv2.createTrackbar('T5','Track',0,800,nothing)
-
 cv2.createTrackbar('T6','Track',0,600,nothing)
 kernel = np.ones((4,4))
 while True:
@@ -33,27 +30,33 @@ while True:
     canny = cv2.Canny(gray,thresh1,thresh2)
     dill = cv2.dilate(canny,kernel,iterations=2)
     new_img = img.copy()
-    contours,h = cv2.findContours(dill,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-    for contour in contours:
-        area = cv2.contourArea(contour)
-       
-        if area > 10000:
-            contours = cv2.drawContours(new_img,contour,-1,(200,200,0),3)
-            print(contours)
-            p = cv2.arcLength(contour,True)
-            # print(p)
-            num = cv2.approxPolyDP(contour,3*p,True)
-            cv2.line(img,(514,193),(610,390),(119,201,105),thickness=3)
-            cv2.line(img,(223,347),(316,543),(119,201,105),thickness=3)
-            cv2.line(img,(223,347),(514,193),(119,201,105),thickness=3)
-            cv2.line(img,(316,543),(610,390),(119,201,105),thickness=3)
-            x,y,w,h = cv2.boundingRect(num)
-            if len(num) == choose:
-                print(x,y,w,h)
-                cv2.rectangle(img,(x,y,x+w,y+h),(0,0,255),3)   
-                parall = ceil(atan(abs((x-y)/1+x*y))*180/pi)
-                if choose == 4 and parall == 90:
-                    cv2.putText(img,'90 degree',(x+w//4,y+h+h//6),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
+    contours,h = cv2.findContours(dill,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    logik = False
+    for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 10000 :
+                rect = cv2.minAreaRect(cnt) 
+                box = cv2.boxPoints(rect) 
+                box = np.intp(box) 
+    cv2.drawContours(img,[box],0,(255,0,0),2)
+    # for contour in contours:
+    #     area = cv2.contourArea(contour)
+    #     if area > 5000:
+    #         contours = cv2.drawContours(new_img,contour,-1,(200,200,0),3)
+    #         print(contours)
+    #         p = cv2.arcLength(contour,True)
+    #         num = cv2.approxPolyDP(contour,0.01*p,True)
+    #         cv2.line(img,(514,193),(610,390),(119,201,105),thickness=3)
+    #         cv2.line(img,(223,347),(316,543),(119,201,105),thickness=3)
+    #         cv2.line(img,(223,347),(514,193),(119,201,105),thickness=3)
+    #         cv2.line(img,(316,543),(610,390),(119,201,105),thickness=3)
+            # x,y,w,h = cv2.boundingRect(num)
+            # if len(num) == choose:
+            #     print(x,y,w,h)
+            #     cv2.rectangle(img,(x,y,x+w,y+h),(0,0,255),3)   
+            #     parall = ceil(atan(abs((x-y)/1+x*y))*180/pi)
+            #     if choose == 4 and parall == 90:
+            #         cv2.putText(img,'90 degree',(x+w//4,y+h+h//6),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
     Hori = np.concatenate((dill, gray), axis=1)
     Hori_2 = np.concatenate((new_img,img), axis=1)
     cv2.imshow('One layer', Hori)

230acdab52c6f8cc182c041615d9c456743063cb
Alex <razuvaev.sasha2015@yandex.ru>
2023-07-31 01:52:16 +0400

    Start work with face-block

--- work_firstday-face-update.py
+++ work_firstday-face-update.py
@@ -13,7 +13,7 @@ def auto_scaleFactor_forBody(turple,num_person):
         cv2.putText(img,'Person',(x+w//4,y+h+h//4),cv2.FONT_HERSHEY_TRIPLEX,0.5,(0,0,255),thickness=1)
 
 
-img = cv2.imread('images/test-upperbody-1.jpg')
+img = cv2.imread('images/work-8.jpg')
 img = cv2.resize(img,(600,400))
 img=cv2.GaussianBlur(img,(3,3),0)
 
@@ -44,23 +44,12 @@ if len(face_coordinates) == 0 and len(body_coordinates)== 0:
                 scaleFact = scaleFact+0.01
                 if(int(scaleFact) == 4):
                     break
-                turple = trained_eye_data.detectMultiScale(grayscaled_img ,scaleFactor=scaleFact,minNeighbors=4)
-                x,y,w,h = eye_coordinates[0]
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 2)
-                cv2.rectangle(img, (x-w*2, y-h*2), (x + w*2, y + h*4), (0,0,255), 2)
-                cv2.putText(img,'Eye',(x+w//4,y+h+h//3),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
-        if(len(eye_coordinates)>1):  
-            x=eye_coordinates[0]
-            x_2=eye_coordinates[1]
-            res = x[0]-x_2[0]
-            if(res > x[3]*3 and res > x_2[3]*3):
-                kernel = np.ones((5,5))
-                canny = cv2.Canny(grayscaled_img,200,50)
-                dill = cv2.dilate(canny,kernel,iterations=1)
-                contours,h = cv2.findContours(dill,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-                for x,y,w,h in eye_coordinates :
-                     cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 2)
-                     cv2.rectangle(img, (x-w*2, y-h*2), (x + w*2, y + h*4), (0,0,255), 2)
-                     cv2.putText(img,'Eye',(x+w//4,y+h+h//3),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
+                eye_coordinates = trained_eye_data.detectMultiScale(grayscaled_img ,scaleFactor=scaleFact,minNeighbors=3)
+        for (x, y, w, h) in eye_coordinates[0:num_of_person]:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0,255,0), 2)
+                            cv2.rectangle(img, (x-w*2, y-h*2), (x + w*2, y + h*4), (0,0,255), 2)
+                            cv2.putText(img,'Eye',(x+w//4,y+h+h//3),cv2.FONT_HERSHEY_TRIPLEX,0.6,(0,255,0),thickness=1)
+
+
 cv2.imshow('Rezult', img)
 cv2.waitKey(0)
